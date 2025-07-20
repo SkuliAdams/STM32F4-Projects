d75452811974e2a45b70dbada21dc452aef356ed
SkuliAdams <jonskuliadams@gmail.com>
2025-07-20 17:15:02 -0400

    Improve waiting method
    Change from using HAL_Delay() to counting ticks.

--- CountUp/Core/Src/main.c
+++ CountUp/Core/Src/main.c
@@ -67,7 +67,8 @@ void MX_USB_HOST_Process(void);
  * @brief  The application entry point.
  * @retval int
  */
-int main(void) {
+int main(void)
+{
 
 	/* USER CODE BEGIN 1 */
 
@@ -96,20 +97,25 @@ int main(void) {
 	MX_SPI1_Init();
 	MX_USB_HOST_Init();
 	/* USER CODE BEGIN 2 */
-
+	uint32_t current_tick = 0;
+	uint32_t tick_last_counted = 0;
 	/* USER CODE END 2 */
 
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
-	while (1) {
-		setPins(counter);
+	while (1)
+	{
+		current_tick = HAL_GetTick();
+		if (current_tick >= (tick_last_counted + 1000))
+		{
+			if (counter == 15)
+				counter = 0;
+			else
+				counter++;
 
-		HAL_Delay(1000);
-
-		if (counter == 15)
-			counter = 0;
-		else
-			counter++;
+			setPins(counter);
+			tick_last_counted = current_tick;
+		}
 
 		/* USER CODE END WHILE */
 		MX_USB_HOST_Process();
@@ -123,9 +129,12 @@ int main(void) {
  * @brief System Clock Configuration
  * @retval None
  */
-void SystemClock_Config(void) {
-	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct =
+	{ 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct =
+	{ 0 };
 
 	/** Configure the main internal regulator output voltage
 	 */
@@ -143,7 +152,8 @@ void SystemClock_Config(void) {
 	RCC_OscInitStruct.PLL.PLLN = 336;
 	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
 	RCC_OscInitStruct.PLL.PLLQ = 7;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
 		Error_Handler();
 	}
 
@@ -156,7 +166,8 @@ void SystemClock_Config(void) {
 	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
 	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+	{
 		Error_Handler();
 	}
 }
@@ -169,11 +180,13 @@ void SystemClock_Config(void) {
  * @brief  This function is executed in case of error occurrence.
  * @retval None
  */
-void Error_Handler(void) {
+void Error_Handler(void)
+{
 	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
-	while (1) {
+	while (1)
+	{
 	}
 	/* USER CODE END Error_Handler_Debug */
 }
